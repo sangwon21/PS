4de4106ecba8c98f14053230597599035451233d
Sangwon <junothyme@gmail.com>
2019-03-30 23:07:51 +0900

    15656

---
+++ BOJ/15656.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+
+int N, M;
+int arr[10];
+int ans[10];
+
+
+void go(int idx) {
+	if (idx == M) {
+		for (int i = 0; i < M; i++) {
+			std::cout << ans[i] << ' ';
+		}
+		std::cout << '\n';
+		
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		ans[idx] = arr[i];
+		go(idx + 1);
+	}
+}
+
+int main(void) {
+
+	std::cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		std::cin >> arr[i];
+	}
+
+	std::sort(arr, arr + N);
+
+	go(0);
+
+	return 0;
+}
