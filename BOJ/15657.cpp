d9627b8c3726cb63abfe1c7bc834f8f1806d566c
Sangwon <junothyme@gmail.com>
2019-03-31 20:11:28 +0900

    15657

---
+++ BOJ/15657.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+int N, M;
+int arr[10];
+int ans[10];
+bool check[10];
+
+void go(int idx, int start) {
+	if (idx == M) {
+		for (int i = 0; i < M; i++) {
+			std::cout << ans[i] << ' ';
+		}
+		std::cout << '\n';
+	
+		return;
+	}
+	for (int i = start; i < N; i++) {
+		ans[idx] = arr[i];
+		go(idx + 1, i);
+	}
+}
+
+int main(void) {
+
+	std::cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		std::cin >> arr[i];
+	}
+
+	std::sort(arr, arr + N);
+
+	go(0, 0);
+	return 0;
+}
