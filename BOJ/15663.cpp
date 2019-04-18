cde5bdfbca53556e480c4e64eaf35012a0bfb50e
sangwon <junothyme@gmail.com>
2019-04-18 21:02:52 +0900

    15663, 2178

--- BOJ/15663.cpp
+++ BOJ/15663.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <algorithm>
 
-int ans[10];
-int num[10];
-int cnt[10];
-
 int N, M;
+int arr[10];
+int cnt[10];
+int num[10];
 
 void go(int idx)
 {
@@ -13,41 +12,40 @@ void go(int idx)
 	{
 		for (int i = 0; i < M; i++)
 		{
-			std::cout << num[ans[i]] << ' ';
+			std::cout << arr[i] << ' ';
 		}
 		std::cout << '\n';
 		return;
 	}
 	for (int i = 0; i < N; i++)
 	{
-		if (cnt[i] > 0)
+		if (cnt[i] >= 1)
 		{
 			cnt[i] -= 1;
-			ans[idx] = i;
+			arr[idx] = num[i];
 			go(idx + 1);
 			cnt[i] += 1;
 		}
 	}
 }
-
 int main(void)
 {
-	int tmp[10];
 	std::cin >> N >> M;
 
 	for (int i = 0; i < N; i++)
 	{
-		std::cin >> tmp[i];
+		std::cin >> arr[i];
 	}
 
-	std::sort(tmp, tmp + N);
+	std::sort(arr, arr + N);
 
 	int c = 1;
 	int k = 0;
-	int x = tmp[0];
+	int x = arr[0];
+
 	for (int i = 1; i < N; i++)
 	{
-		if (x == tmp[i])
+		if (x == arr[i])
 		{
 			c += 1;
 		}
@@ -56,7 +54,7 @@ int main(void)
 			num[k] = x;
 			cnt[k] = c;
 			k += 1;
-			x = tmp[i];
+			x = arr[i];
 			c = 1;
 		}
 	}
@@ -64,6 +62,8 @@ int main(void)
 	num[k] = x;
 	cnt[k] = c;
 	N = k + 1;
+
 	go(0);
+
 	return 0;
 }
--- BOJ/2178.cpp
+++ BOJ/2178.cpp
@@ -2,66 +2,70 @@
 #include <iostream>
 #include <queue>
 
-using namespace std;
-
-int n, m;
-int map[101][101];
-bool check[101][101];
+int N, M;
+int map[100][100];
+bool check[100][100];
 int dx[4] = { -1,1,0,0 };
 int dy[4] = { 0,0,-1,1 };
-typedef struct {
-	int x;
+int ans = 0;
+
+struct loc {
 	int y;
-}Pos;
+	int x;
+};
+void go(int y, int x)
+{
+	using namespace std;
 
-bool isValid(int x, int y) {
-	return 0 <= x && x < n && 0 <= y && y < m;
-}
-void bfs() {
-	queue<pair<Pos, int>> q;
-	Pos start;
-	start.x = 0;
-	start.y = 0;
-	q.push(make_pair(start, 0));
-	check[start.x][start.y] = true;
+	queue<pair<loc, int>> q;
+	q.push(make_pair(loc{ y, x }, 1));
+	check[y][x] = true;
 
-	while (!q.empty()) {
-		int curX = q.front().first.x;
-		int curY = q.front().first.y;
-		int dist = q.front().second;
+	while (!q.empty())
+	{
+		int currentY = q.front().first.y;
+		int currentX = q.front().first.x;
+		int distance = q.front().second;
 		q.pop();
 
-		if (curX == n - 1 && curY == m - 1) {
-			cout << dist + 1 << endl;
+		if (currentY == N - 1 && currentX == M - 1)
+		{
+			ans = distance;
 			break;
 		}
+		for (int i = 0; i < 4; i++)
+		{
+			int nextY = currentY + dy[i];
+			int nextX = currentX + dx[i];
 
-		for (int i = 0; i < 4; i++) {
-			int newX = curX + dx[i];
-			int newY = curY + dy[i];
-
-			if (isValid(newX, newY)) {
-				if (map[newX][newY] == 1 && check[newX][newY] == false) {
-					check[newX][newY] = true;
-					Pos pos;
-					pos.x = newX;
-					pos.y = newY;
-					q.push(make_pair(pos, dist + 1));
+			if (0 <= nextY && nextY < N && 0 <= nextX && nextX < M)
+			{
+				if (check[nextY][nextX] == false && map[nextY][nextX] == 1)
+				{
+					check[nextY][nextX] = true;
+					q.push(make_pair(loc{ nextY, nextX }, distance + 1));
 				}
 			}
 		}
 	}
 }
-int main(void) {
-	
-	cin >> n >> m;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			scanf("%1d", &map[i][j]);
+
+int main(void)
+{
+	std::cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			scanf("%1d",&map[i][j]);
 		}
 	}
 
-	bfs();
+	go(0, 0);
+	
+	std::cout << ans << '\n';
+
 	return 0;
 }
