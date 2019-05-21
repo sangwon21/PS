297bd885cd65cea8fc42b82ff5311e6337191163
junothyme <junothyme@gmail.com>
2019-05-21 13:31:47 +0900

    709. To Lower Case

---
+++ Leetcode/709_To_Lower_Case.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for(int i = 0; i < str.size(); i++)
+        {
+            if(str[i] >= 'A' && str[i] <= 'Z')
+            {
+                str[i] = (str[i] - 'A') + 'a';
+            }
+        }
+        
+        return str;
+    }
+};
