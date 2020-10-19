dafa88f7d1da6d2210fbd96e1b573bdc33175fe7
sangwon <junothyme@gmail.com>
2020-10-19 23:12:31 +0900

    feat: 49. Group Anagrams

---
+++ Leetcode/49_Group_Anagrams.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const anagrams = {};
+    
+    strs.forEach(str => {
+        const sortedStr = str.split('').sort().join('');
+        if (!anagrams[sortedStr]) {
+            anagrams[sortedStr] = [str];
+            return;
+        }
+        anagrams[sortedStr].push(str);
+    });
+    
+    const ans = [];
+    
+    for (anagram in anagrams) {
+        ans.push(anagrams[anagram]);
+    }
+    return ans;
+};
