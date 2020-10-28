b9be3cbe2675fc73ae7ec0893fde19f51ca04b8c
sangwon <junothyme@gmail.com>
2020-10-28 13:24:57 +0900

    feat: Best Time to Buy and Sell Stock

---
+++ Leetcode/121_Best_Time_to_Buy_and_Sell_Stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice;
+    }
+    if (minPrice > prices[i]) {
+      minPrice = prices[i];
+    }
+  }
+  return maxProfit;
+};
