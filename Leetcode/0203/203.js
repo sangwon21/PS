175758b2d75fb0c51c0f4ac99a896ac28cc5d2a1
sangwon <junothyme@gmail.com>
2021-03-04 21:45:54 +0900

    203번 풀이

---
+++ Leetcode/0203/203.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+	if (!head) {
+		return null;
+	}
+
+	head.next = removeElements(head.next, val);
+
+	if (head.val === val) {
+		return head.next;
+	}
+	return head;
+};
