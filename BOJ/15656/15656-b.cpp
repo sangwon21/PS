e76f4c0759cb25b6644c901d4dd2c63703433280
sangwon <junothyme@gmail.com>
2021-08-09 22:44:38 +0900

    BOJ 15656

---
+++ BOJ/15656/15656-b.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+int n, m;
+int arr[9];
+int tmp[9];
+
+void traverse(int count)
+{
+    if (count == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            std::cout << tmp[i] << ' ';
+        }
+        std::cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        tmp[count] = arr[i];
+        traverse(count + 1);
+    }
+}
+
+int main()
+{
+    std::cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> arr[i];
+    }
+
+    std::sort(arr, arr + n);
+
+    traverse(0);
+    return 0;
+}
