9ceaaea4cca3e3e22abf1f0d16b84d95a3be067d
sangwon <junothyme@gmail.com>
2021-08-10 08:51:24 +0900

    BOJ 15665

---
+++ BOJ/15665/15665-b.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+
+int n, m;
+int arr[9];
+int count[9];
+int number[9];
+
+void traverse(int selected)
+{
+    if (selected == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            std::cout << arr[i] << ' ';
+        }
+        std::cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+
+        arr[selected] = number[i];
+        traverse(selected + 1);
+    }
+}
+
+int main()
+{
+    std::cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> arr[i];
+    }
+
+    std::sort(arr, arr + n);
+
+    int c = 1;
+    int target = arr[0];
+    int idx = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (target == arr[i])
+        {
+            c++;
+        }
+        else
+        {
+            count[idx] = c;
+            number[idx] = target;
+            target = arr[i];
+            c = 1;
+            idx++;
+        }
+    }
+
+    count[idx] = c;
+    number[idx] = target;
+    n = idx + 1;
+
+    traverse(0);
+
+    return 0;
+}
