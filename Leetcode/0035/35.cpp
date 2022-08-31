23d458156b28b6dae94119307fb831b6c857f9a4
sangwon <junothyme@gmail.com>
2022-08-31 17:21:29 +0900

    35. Search Insert Position

---
+++ Leetcode/0035/35.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int left = 0;
+        int right = nums.size();
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+
+            if (nums[mid] > target)
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+};
